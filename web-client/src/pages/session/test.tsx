477ea9e7d72572138a632f2ee87a79d21c7aa37a
nimendoza <lunar_bloom@ncisomendoza.localdomain>
2022-08-03 17:13:15 +0800

    implemented suggested changes

--- web-client/src/pages/session/test.tsx
+++ web-client/src/pages/session/test.tsx
@@ -5,41 +5,64 @@ import { User } from './types';
 import { Meta } from '../../layout/Meta';
 import { Main } from '../../templates/Main';
 import { AppConfig } from '../../utils/AppConfig';
+import { useAsyncEffect } from 'use-async-effect';
 
-const LoginTest = () => {  
-  const userJson = localStorage.getItem(User.CURRENT);
-  const jwt = localStorage.getItem(User.JWT);
+const LoginTest_ = () => {  
+  const getCurrentUser = async () => {
+    const userJson = localStorage.getItem(User.CURRENT);
+    const jwt = localStorage.getItem(User.JWT);
 
-  if (userJson == null || jwt == null) {
-    return (
-      <Main meta={<Meta title={AppConfig.title} description={AppConfig.description}/>}>
-        You are logged out
-      </Main>
-    )
-  }
+    if (userJson == null || jwt == null) {
+      return null;
+    }
+
+    const user = JSON.parse(userJson);
+    try {
+      await axios.get(`http://localhost:4000/v1/users/${user.id}`, {
+        headers: {
+          'Authorization': jwt
+        }
+      });
+      return user;
+    } catch (err) {
+      return null;
+    }
+  };
+
+  const [status, setStatus] = React.useState('loading');
+  const [user, setUser] = React.useState(null);
+
+  const checkIfLoggedInAndUpdateStatus = async () => {
+    const user_ = await getCurrentUser();
+    if (user_ != null) {
+      setStatus('logged-in');
+      setUser(user_);
+    } else {
+      setStatus('logged-out');
+    }
+  };
+
+  useAsyncEffect(checkIfLoggedInAndUpdateStatus);
 
   try {
-    const user = JSON.parse(userJson);
-    (async () => await axios.get(`http://localhost:4000/v1/users/${user.id}`, {
-      headers: {
-        'Authorization': jwt
-      }
-    }));
-    return (
-      <Main meta={<Meta title={AppConfig.title} description={AppConfig.description}/>}>
-        You are logged in as {user.email}
-      </Main>
-    )
+    if (status == 'loading') {
+      return <div>Loading</div>;
+    } else if (status == 'logged-in') {
+      return <div>You are logged in as {user.email}</div>;
+    } else if (status == 'logged-out') {
+      return <div>You are logged out</div>;
+    }
   } catch (err) {
-    alert('Your JWT token has expired! Logging out...');
-    localStorage.removeItem(User.CURRENT);
-    localStorage.removeItem(User.JWT);
-    return (
-      <Main meta={<Meta title={AppConfig.title} description={AppConfig.description}/>}>
-        You are logged out
-      </Main>
-    )
+    return <div>Something unexpected happened</div>
   }
+}
+
+const LoginTest = () => {
+  return (
+    <Main meta={<Meta title={AppConfig.title} description={AppConfig.description}/>}>
+      <LoginTest_/>
+    </Main>
+  )
 }
 
 export default LoginTest
