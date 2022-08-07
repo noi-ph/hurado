6cc82c89b81770614da9922d6d2bcce4d73ec3d5
nimendoza <ncisomendoza@gmail.com>
2022-08-07 19:07:54 +0800

    added ViewPage & EditPage;

--- web-client/src/pages/session/types.ts
+++ web-client/src/pages/session/types.ts
@@ -1,5 +1,14 @@
 export type User = {
+  id: number;
   email: string;
+  username: string;
+  createdAt: Date;
+  isAdmin: boolean;
+  school: string;
+  firstName: string;
+  lastName: string;
+  country: string;
+  updatedAt: Date;
 };
 export enum UserConstants {
   CURRENT = 'currentUserJson',
