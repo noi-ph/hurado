c07656a32b15280f4bea46252ed3e05e2de2e601
tdserapio <monsterbob2008@gmail.com>
2022-08-07 14:13:13 +0800

    Fixed User and File migrations

--- web-server/src/types/JwtPayload.ts
+++ web-server/src/types/JwtPayload.ts
@@ -1,9 +1,7 @@
-import { Role } from '../orm/entities/users/types';
-
 export type JwtPayload = {
   id: number;
   name: string;
   email: string;
-  role: Role;
-  created_at: Date;
+  isAdmin: boolean;
+  createdAt: Date;
 };
