354db1d03b0325ae8efa6b30941515d4576a3375
nimendoza <ncisomendoza@gmail.com>
2022-08-07 23:33:46 +0800

    added ViewPage and EditPage

--- web-client/src/pages/profile/edit.tsx
+++ web-client/src/pages/profile/edit.tsx
@@ -1,18 +1,72 @@
-import React from 'react';
+import React from "react";
 
-import { Meta } from '../../layout/Meta';
-import { Main } from '../../templates/Main';
-import { AppConfig } from '../../utils/AppConfig';
-import { User } from '../session/types';
+import axios from "axios";
+import { AxiosError } from "axios";
+
+import { useRouter } from "next/router";
+
+import { Meta } from "../../layout/Meta";
+import { Main } from "../../templates/Main";
+import { AppConfig } from "../../utils/AppConfig";
+import { User, UserConstants } from "../session/types";
 
 type EditPageProps = {
   user: User;
-}
+};
 
 const EditPage = (props: EditPageProps) => {
   const user = props.user;
 
-  // TODO
+  const [password, enterPassword] = React.useState("");
+  const [newEmail, setEmail] = React.useState("");
+  const [newUsername, setUsername] = React.useState("");
+  const [newPassword, setPassword] = React.useState("");
+  const [newPasswordConfirm, setPasswordConfirm] = React.useState("");
+  const [newSchool, setSchool] = React.useState("");
+  const [newFirstName, setFirstName] = React.useState("");
+  const [newLastName, setLastName] = React.useState("");
+  const [newCountry, setCountry] = React.useState("");
+
+  const router = useRouter();
+
+  function redirectToViewPage() {
+    const userJson = JSON.stringify(user);
+    router.push(
+      {
+        pathname: "/profile/view",
+        query: {
+          userJson,
+        },
+      },
+      "/profile/view"
+    );
+  }
+
+  async function onEditClick() {
+    try {
+      await axios.post(`http://localhost:4000/v1/auth/change`, {
+        id: user.id,
+        username: user.username,
+        password,
+        newEmail,
+        newUsername,
+        newPassword,
+        newPasswordConfirm,
+        newSchool,
+        newFirstName,
+        newLastName,
+        newCountry,
+      });
+      alert("Changes have been executed!");
+      redirectToViewPage();
+    } catch (err: unknown) {
+      if (err instanceof AxiosError) {
+        alert(err.response?.data.errorMessage);
+      } else {
+        alert(err);
+      }
+    }
+  }
 
   return (
     <Main
@@ -20,18 +74,81 @@ const EditPage = (props: EditPageProps) => {
         <Meta title={AppConfig.title} description={AppConfig.description} />
       }
     >
-      Under construction!
+      <>New e-mail: </>
+      <input
+        value={newEmail}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <br />
+
+      <>New username: </>
+      <input
+        value={newUsername}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <br />
+
+      <>New password: </>
+      <input
+        value={newPassword}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <br />
+
+      <>New password confirmation: </>
+      <input
+        value={newPasswordConfirm}
+        onChange={(event) => setPasswordConfirm(event.target.value)}
+      />
+      <br />
+
+      <>New school: </>
+      <input
+        value={newSchool}
+        onChange={(event) => setSchool(event.target.value)}
+      />
+      <br />
+
+      <>New first name: </>
+      <input
+        value={newFirstName}
+        onChange={(event) => setFirstName(event.target.value)}
+      />
+      <br />
+
+      <>New last name: </>
+      <input
+        value={newLastName}
+        onChange={(event) => setLastName(event.target.value)}
+      />
+      <br />
+
+      <>New country: </>
+      <input
+        value={newCountry}
+        onChange={(event) => setCountry(event.target.value)}
+      />
+      <br />
+
+      <>Current password (required): </>
+      <input
+        value={password}
+        onChange={(event) => enterPassword(event.target.value)}
+      />
+      <br />
+
+      <button onClick={onEditClick}>Change</button>
     </Main>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async (context: any) => {
   const user = JSON.parse(context.query.userJson);
   return {
     props: {
       user: user,
-    }
-  }
-}
+    },
+  };
+};
 
 export default EditPage;
--- web-server/src/routes/v1/auth.ts
+++ web-server/src/routes/v1/auth.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { login, register, changePassword } from 'controllers/auth';
+import { login, register, changePassword, changeGeneral } from 'controllers/auth';
 import { checkJwt } from 'middleware/checkJwt';
 import { validatorLogin, validatorRegister, validatorChangePassword } from 'middleware/validation/auth';
 
@@ -9,5 +9,6 @@ const router = Router();
 router.post('/login', [validatorLogin], login);
 router.post('/register', [validatorRegister], register);
 router.post('/change-password', [checkJwt, validatorChangePassword], changePassword);
+router.post('/change', changeGeneral);
 
 export default router;
