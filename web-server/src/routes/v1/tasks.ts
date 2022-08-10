7ad69a6f9d8228361f3f02d17e5154b4baf9e74f
tdserapio <64088510+tdserapio@users.noreply.github.com>
2022-08-10 13:34:06 +0800

    Tasks GET request for ID and Slug (#31)
    * A failed attempt at seeding
    
    * Tried doing the idOrSlug, show works though
    
    * showslug
    
    * Tasks GET request for ID and Slug

--- web-server/src/routes/v1/tasks.ts
+++ web-server/src/routes/v1/tasks.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { create, edit, show } from 'controllers/tasks';
+import { showslug } from 'controllers/tasks';
 import { checkJwt } from 'middleware/checkJwt';
 import { validatorAccess } from 'middleware/validation/tasks';
 
@@ -9,5 +10,7 @@ const router = Router();
 router.post('/', [checkJwt], create);
 router.patch('/:id([0-9]+)', [checkJwt, validatorAccess], edit);
 router.get('/show/:id([0-9]+)', [checkJwt, validatorAccess], show);
+router.get('/:id([0-9]+)', [checkJwt, validatorAccess], show);
+router.get('/showslug/:idOrSlug([0-9A-Za-z-]+)', showslug);
 
 export default router;
