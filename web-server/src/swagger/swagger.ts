ab5d29d324376e9d10f84630442f15638f78ff9b
nimendoza <ncisomendoza@gmail.com>
2022-08-11 23:14:20 +0800

    discarded used files

--- web-server/src/swagger/swagger.ts
+++
@@ -1,6 +0,0 @@
-import swaggerAutogen from 'swagger-autogen';
-
-const outputFile = 'src/swagger/swagger_output.json';
-const endpointsFiles = ['src/routes/v1/*.ts'];
-
-swaggerAutogen()(outputFile, endpointsFiles);
