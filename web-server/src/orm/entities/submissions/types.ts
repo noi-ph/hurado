8264a330b3218ac10bad3dffb9ce6bfb7a64dd2d
Kian <kiancnchua@gmail.com>
2022-08-11 14:03:10 +0800

    added result

---
+++ web-server/src/orm/entities/submissions/types.ts
@@ -0,0 +1,15 @@
+export enum Verdicts {
+  IcpcStyle = 'ICPC-Style',
+  Batch = 'Batch',
+  OutputOnly = 'Output-only',
+  Communication = 'Communication',
+  Custom = 'Custom',
+  Queued = 'Queued',
+  Running = 'Running',
+  Accepted = 'Accepted',
+  TLE = 'TLE',
+  RTE = 'RTE',
+  MLE = 'MLE',
+  CE = 'CE',
+  Skipped = 'Skipped',
+}
