4b6bf625cc0d091cca47137bc256309627499f98
nimendoza <ncisomendoza@gmail.com>
2022-11-28 20:08:32 +0800

    Re-arrange `idOrSlug.tsx`

--- web-server/src/middleware/validation/tasks/idOrSlug.ts
+++ web-server/src/middleware/validation/tasks/idOrSlug.ts
@@ -5,16 +5,15 @@ import { TaskRepository } from 'orm/repositories';
 export const idOrSlug = async (req: Request, res: Response, next: NextFunction) => {
   const idOrSlug = req.params.idOrSlug;
 
-  console.log('hello!!', idOrSlug);
-  let task = await TaskRepository.findOne({ where: { id: idOrSlug } });
-  if (task) {
-    req.params['id'] = idOrSlug;
-    return next();
-  }
-
-  task = await TaskRepository.findOne({ where: { slug: idOrSlug } });
+  let task = await TaskRepository.findOne({ where: { slug: idOrSlug } });
   if (task) {
     req.params['id'] = task.id;
+    return next();
+  } 
+
+  task = await TaskRepository.findOne({ where: { id: idOrSlug } });
+  if (task) {
+    req.params['id'] = idOrSlug;
     return next();
   } else {
     res.status(404).end();
