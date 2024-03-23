19b412be671239bec7dc9c9a808e62b78e1ac2ee
Neomi Mendoza <ncisomendoza@gmail.com>
2024-03-23 04:27:12 +0000

    modify auth hooks

--- app/_hooks/auth/index.tsx
+++ app/_hooks/auth/index.tsx
@@ -2,35 +2,96 @@
 
 import type { User } from '@models'
 
-import { useState, useEffect } from 'react'
-import { cookies } from 'next/headers'
+import { useState, useEffect, useCallback } from 'react'
+import { getCookie, setCookie, deleteCookie } from 'cookies-next'
 
-export const useAuth = () => {
-    const [ user, setUser ] = useState<User | null>(null)
-    const [ loggedin, setLoggedin ] = useState<boolean>(false)
-
-    const storage = cookies()
-    const token = storage.get('algurado/token')?.value
+const useToken = () => {
+    const data = getCookie('algurado/token')
+    const [ token, setToken ] = useState<string | null>(data ?? null)
 
     useEffect(() => {
-        (async () => {
-            try {
-                const response = await fetch('api/v1/auth', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`,
-                    },
-                })
-    
-                if (response.ok) {
-                    const data = await response.json()
-                    setUser(data.user!)
-                    setLoggedin(true)
-                }
-            } catch (error) {}
-        })()
-    }, [])
+        if (token === null) {
+            return deleteCookie('algurado/token')
+        }
 
-    return { user, setUser, loggedin, setLoggedin }
+        setCookie('algurado/token', token)
+    }, [ token ])
+
+    return { token, setToken }
+}
+
+const useUser = () => {
+    const data = getCookie('algurado/user')
+    const [ user, setUser ] = useState<User | null>(
+        data ? JSON.parse(data) : null
+    )
+
+    useEffect(() => {
+        if (user === null) {
+            return deleteCookie('algurado/user')
+        }
+
+        setCookie('algurado/user', user)
+    }, [ user ])
+
+    return { user, setUser }
+}
+
+const validate = async () => {
+    const { setUser } = useUser()
+    const { token, setToken } = useToken()
+
+    try {
+        const response = await fetch('api/v1/auth', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        })
+
+        if (response.ok) {
+            const data = await response.json()
+
+            setUser(data.user as User)
+            setToken(
+                response.headers.get('Authorization')?.split(' ')[1] ?? null
+            )
+
+            return
+        }
+    } catch (error) {}
+
+    setUser(null)
+    setToken(null)
+
+    return
+}
+
+const useValidate = useCallback(async () => {
+    await validate()
+}, [ validate ])
+
+export const useAuth = () => {
+    const { user } = useUser()
+    const { token, setToken } = useToken()
+    const [ shouldCheck, setShouldCheck ] = useState<boolean>(false)
+
+    useEffect(() => {
+        useValidate()
+    }, [ token ])
+
+    useEffect(() => {
+        if (shouldCheck) {
+            useValidate()
+            setShouldCheck(false)
+        }
+    }, [ shouldCheck ])
+
+    return {
+        user,
+        token,
+        setToken,
+        checkAuth: () => setShouldCheck(true),
+    }
 }
