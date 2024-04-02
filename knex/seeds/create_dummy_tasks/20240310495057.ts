86e2f865ba45b9c187c488cd33c9f4f069d07f81
Neomi Mendoza <56363312+nimendoza@users.noreply.github.com>
2024-04-02 14:32:35 +0800

    add Tasks CRUD (no MathJax support yet) (#76)
    * add knex files and types
    
    * add api routing
    
    * add frontend files
    
    * add npm updated packages

---
+++ knex/seeds/create_dummy_tasks/20240310495057.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+ 
+export async function seed(knex: Knex): Promise<void> {
+    await knex('tasks').insert([{
+        slug: 'who-is-the-oldest',
+        title: 'Who is the oldest?',
+        statement: [
+            'Alvin, Berto, and Carlo are friends. Their ages are $$A$$, $$B$$ and $$C$$, respectively. No two of them have the same age. Who is the oldest among them?',
+            'The input contains three lines. The first line contains a single integer, $$A$$. The second line contains a single integer, $$B$$. The third line contains a single integer, $$C$$.',
+            'Output the name of the oldest among the three, which should be either Alvin, Berto or Carlo.'
+        ].join('\n')
+    }])
+}
