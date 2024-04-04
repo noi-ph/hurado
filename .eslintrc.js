4c84889b8d4ffef01e35bd48eddd0464da19bf25
Payton Yao <payton.yao@gmail.com>
2024-04-04 15:38:13 +0800

    Install prettier and update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,46 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
   },
-  extends: [
-    'eslint:recommended',
-    'next/core-web-vitals',
-    'airbnb',
-    'airbnb/hooks',
-  ],
+  extends: ["eslint:recommended", "next/core-web-vitals", "prettier"],
   rules: {
-    'no-console': ['warn', {
-      allow: ['info', 'warn', 'error'],
-    }],
-    'no-empty': 'off',
-    'consistent-return': 'off',
-    'import/extensions': ['error', 'never'],
-    'import/prefer-default-export': 'off',
-    'jsx-a11y/label-has-associated-control': 'off',
-    'react/function-component-definition': 'off',
-    'react/jsx-filename-extension': ['error', {
-      extensions: ['.jsx', '.tsx'],
-      ignoreFilesWithoutCode: true,
-    }],
-    'react/react-in-jsx-scope': 'off',
+    "no-console": [
+      "warn",
+      {
+        allow: ["info", "warn", "error"],
+      },
+    ],
+    "no-empty": "off",
+    "consistent-return": "off",
+    "import/extensions": ["error", "never"],
+    "import/no-unresolved": "off", // Let the typescript compiler handle module resolution
+    "import/order": [
+      "warn",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+        ],
+        "alphabetize": { "caseInsensitive": false },
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "react/function-component-definition": "off",
+    "react/jsx-filename-extension": [
+      "error",
+      {
+        extensions: [".jsx", ".tsx"],
+        ignoreFilesWithoutCode: true,
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
   },
 };
