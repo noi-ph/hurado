6291cbe0c90574eaa61965d7f46f9d82fb74e2c0
Payton Yao <payton.yao@gmail.com>
2024-08-19 00:06:16 +0800

    Align text in subtask score title

--- src/client/components/submission_viewer/submission_viewer.tsx
+++ src/client/components/submission_viewer/submission_viewer.tsx
@@ -127,7 +127,7 @@ const SubtaskVerdictViewer = ({ subtask, subtaskIndex }: SubtaskVerdictViewerPro
 
   return (
     <div className="mt-4 first:mt-0">
-      <div className="flex px-4 py-2 rounded-t-lg bg-blue-300">
+      <div className="flex items-center px-4 py-2 rounded-t-lg bg-blue-300">
         <div className="text-lg">Subtask #{subtaskIndex + 1}</div>
         <div className="ml-auto">Score: {score}</div>
       </div>
