2a1e4377efa586fc626b88d17f1d421636192081
Payton Yao <payton.yao@gmail.com>
2025-01-12 21:10:43 +0800

    Make navbar work better on mobile

--- src/client/components/navbar/index.tsx
+++ src/client/components/navbar/index.tsx
@@ -2,25 +2,63 @@
 
 import classNames from "classnames";
 import Link from "next/link";
-import { memo } from "react";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
 import { useSession } from "client/sessions";
 import { getPath, Path } from "client/paths";
+import HamburgerSVG from "assets/icons/hamburger.svg";
 
 type NavbarProps = {
   className?: string;
 };
 
 export const Navbar = memo(({ className }: NavbarProps) => {
-  const session = useSession();
-  const loggedOut = session == null || session.user == null;
+  const [isOpen, setIsOpen] =  useState(false);
+
+  const linksRef = useRef<HTMLDivElement>(null);
+
+  const toggleOpen = useCallback(() => {
+    console.log('Toggling open', isOpen);
+    const open = !isOpen;
+    setIsOpen(open);
+    if (linksRef.current) {
+      linksRef.current.style.height = open ? `${linksRef.current.scrollHeight}px` : "0px";
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    const onResize = () => {
+      if (linksRef.current) {
+        linksRef.current.style.height = '';
+      }
+      setIsOpen(false);
+    }
+    window.addEventListener('resize', onResize);
+    return () => {
+      window.removeEventListener('resize', onResize);
+    };
+  }, []);
 
   return (
     <div className={classNames("flex bg-blue-400 text-white", className)}>
-      <div className="flex items-center gap-2 w-full max-w-[64rem] px-4 mx-auto">
-        <NavbarLink href={getPath({ kind: Path.Home })}>Home</NavbarLink>
-        <NavbarLink href={getPath({ kind: Path.ProblemSetList })}>Problems</NavbarLink>
-        <NavbarLink href={getPath({ kind: Path.ContestList })}>Contests</NavbarLink>
-        <NavbarAccount />
+      <div className="lg:flex lg:items-center lg:gap-2 w-full max-w-[64rem] px-4 py-3 mx-auto">
+        <div className="flex justify-between items-center">
+          <NavbarLink href={getPath({ kind: Path.Home })}>Home</NavbarLink>
+          <button
+            type="button"
+            className="text-white rounded border-2 hover:opacity-70 focus:outline-none lg:hidden"
+            onClick={toggleOpen}
+          >
+            <HamburgerSVG className="w-8 h-8" />
+          </button>
+        </div>
+        <div
+          className="h-0 lg:h-auto flex flex-col items-start lg:flex-row lg:items-center gap-2 flex-1 box-border overflow-hidden transition-[height] duration-300 ease-in-out"
+          ref={linksRef}
+        >
+          <NavbarLink href={getPath({ kind: Path.ProblemSetList })}>Problems</NavbarLink>
+          <NavbarLink href={getPath({ kind: Path.ContestList })}>Contests</NavbarLink>
+          <NavbarAccount />
+        </div>
       </div>
     </div>
   );
@@ -31,21 +69,21 @@ export const NavbarAccount = memo(() => {
   if (session == null || session.user == null) {
     return (
       <>
-        <NavbarLink href={getPath({kind: Path.AccountLogin })} className="ml-auto">Login</NavbarLink>
+        <NavbarLink href={getPath({kind: Path.AccountLogin })} className="lg:ml-auto">Login</NavbarLink>
         <NavbarLink href={getPath({kind: Path.AccountRegister })}>Register</NavbarLink>
       </>
     );
   } else if (session.user.role === "admin") {
     return (
       <>
-        <NavbarLink href={getPath({kind: Path.AdminHome })} className="ml-auto">{session.user.name}</NavbarLink>
+        <NavbarLink href={getPath({kind: Path.AdminHome })} className="lg:ml-auto">{session.user.name || 'Anonymous'}</NavbarLink>
         <NavbarLink href={getPath({kind: Path.AccountLogout })}>Logout</NavbarLink>
       </>
     );  
   } else {
     return (
       <>
-        <div className="text-2xl px-1 py-3 ml-auto">{session.user.name}</div>
+        <div className="text-2xl px-1 lg:ml-auto">{session.user.name || 'Anonymous'}</div>
         <NavbarLink href={getPath({kind: Path.AccountLogout })}>Logout</NavbarLink>
       </>
     );  
@@ -60,7 +98,7 @@ type NavbarLinkProps = {
 
 const NavbarLink = ({ href, className, children }: NavbarLinkProps) => {
   return (
-    <Link className={classNames("text-2xl px-1 py-3 hover:text-blue-200", className)} href={href}>
+    <Link className={classNames("text-2xl hover:text-blue-200", className)} href={href}>
       {children}
     </Link>
   );
