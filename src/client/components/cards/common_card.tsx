417eba3934aaf822070a9e23e2190a68c6ed7dcc
Cisco <gfmortega@hotmail.com>
2025-01-21 14:22:04 +0800

    fixed viewing of task cards in mobile
    also, changed the code to be more tailwind-y

--- src/client/components/cards/common_card.tsx
+++ src/client/components/cards/common_card.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { ContestSummaryDTO, ProblemSetSummaryDTO, TaskScoredSummaryDTO, TaskSummaryDTO } from "common/types";
 import { getPath, Path } from "client/paths";
 import classNames from "classnames";
-import styles from "./task_card.module.css";
 
 type CommonCardProps = {
   url: string;
@@ -66,35 +65,39 @@ type TaskCardProps = {
 export function TaskCard({ task }: TaskCardProps) {
   const url = getPath({ kind: Path.TaskView, slug: task.slug });
 
-  let top_class = classNames(styles["card-top"], "group-hover:bg-gray-150");
-  let pbar_class = classNames(styles["card-bar"], styles["card-bar-default"]);
-  let pbar_style = {width: "0%"};
+  let top_class = "p-[1rem] border-t border-l border-r border-black rounded-tl-2xl rounded-tr-2xl group-hover:bg-gray-150";
+  let bottom_class = "p-0 bg-[#cccccc] border-l border-r border-b border-black rounded-bl-2xl rounded-br-2xl overflow-hidden";
+  let pbar_class = "p-[0.25rem]";
+  let pbar_style = {};
 
   if (task.score_overall == null || task.score_max == null) {   // no attempts
-    // pass
-  } else if (task.score_overall == 0 || task.score_max == 0) {  // has attempts, binary score
-    pbar_class = classNames(styles["card-bar"], "bg-yellow-800")
-    pbar_style = { width: "100%" };
-  } else { // partial score or accepted
-    pbar_class = classNames(styles["card-bar"], "bg-green-400")
     pbar_style = {
-      width: `${Math.min(100, Math.max(0, task.score_overall/task.score_max*100))}%`,
+      'width': '0%',
     };
-
-    if (task.score_overall == task.score_max)
-    {
-      top_class = classNames(styles["card-top"], "bg-green-100 group-hover:bg-green-200");
+  } else if (task.score_overall == 0 || task.score_max == 0) {  // has attempts, binary score
+    pbar_class = classNames(pbar_class, "w-full bg-yellow-800");
+    pbar_style = {
+      'width': '100%',
+    };
+  } else { // partial score or accepted
+    pbar_class = classNames(pbar_class, `bg-green-400`);
+    pbar_style = {
+      // this computed value isn't a literal that can be parsed by tailwind, so it has to be a separate thing
+      width: `${Math.max(0, Math.min(100, Math.floor(task.score_overall/task.score_max*100)))}%`,
+    };
+    if (task.score_overall == task.score_max) {
+      top_class = classNames(top_class, "bg-green-100 group-hover:bg-green-200");
     } 
   }
   return (
-    <Link className={classNames("group")} key={task.slug} href={url} >
+    <Link className="w-[96rem] max-w-full group" key={task.slug} href={url} >
       <div className={top_class}>
         <h2 className="text-2xl mb-1">{task.title}</h2>
         <p className="font-light">
           {task.description ?? "No description was provided for this task."}
         </p>
       </div>
-      <div className={styles["card-bottom"]}>
+      <div className={bottom_class}>
         <div className={pbar_class} style={pbar_style}></div>
       </div>
     </Link>
