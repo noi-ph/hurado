88ec63f9c9dbe111416286e5165fd1be2a241025
Payton Yao <payton.yao@gmail.com>
2025-03-01 02:20:34 +0800

    Try fixing worker retry logic

--- src/worker/queue.ts
+++ src/worker/queue.ts
@@ -1,12 +1,20 @@
 import { Queue } from "bullmq";
-import { StandaloneConnectionOptions } from "ioredis";
+import Redis from "ioredis";
 import { REDIS_HOST, REDIS_PORT } from "server/secrets";
 import { __MAIN_QUEUE_NAME__, JobName, PasswordResetData, SubmissionJudgementData } from "./types";
 
-const connection: StandaloneConnectionOptions = {
+const connection = new Redis({
   host: REDIS_HOST,
   port: REDIS_PORT,
-};
+  retryStrategy(times) {
+    return Math.min(times * 100, 3000); // Max wait 3s
+  },
+  reconnectOnError(err) {
+    console.error("Redis error:", err);
+    return true; // Force reconnection on error
+  },
+  maxRetriesPerRequest: 20, // Try every command 20 times before failing
+});
 
 export const JobQueue = new Queue(__MAIN_QUEUE_NAME__, { connection });
 
--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -1,18 +1,30 @@
 import { Worker, Job } from 'bullmq';
-import { StandaloneConnectionOptions } from 'ioredis';
+import Redis from 'ioredis';
 import { UnreachableDefault } from 'common/errors';
 import { REDIS_HOST, REDIS_PORT } from 'server/secrets';
 import { judgeSubmission } from 'server/logic/submissions/judge_submission';
 import { resetPassword } from 'server/logic/users';
 import { __MAIN_QUEUE_NAME__, JobName, PasswordResetData, SubmissionJudgementData } from './types';
 
-const connection: StandaloneConnectionOptions = {
+const connection = new Redis({
   host: REDIS_HOST,
   port: REDIS_PORT,
-};
+  retryStrategy(times) {
+    return Math.min(times * 100, 3000); // Max wait 3s
+  },
+  reconnectOnError(err) {
+    console.error("Redis error:", err);
+    return true; // Force reconnection on error
+  },
+  maxRetriesPerRequest: null, // Keep trying to reconnect forever
+});
 
 // Create a new worker that processes jobs from the 'my-queue' queue
-const worker = new Worker(__MAIN_QUEUE_NAME__, handleIncomingJob, { connection });
+const worker = new Worker(__MAIN_QUEUE_NAME__, handleIncomingJob, {
+  connection,
+  lockDuration: 120000, // 120s
+  maxStalledCount: 3, // Only allow 3 stalled job before marking it as failed
+});
 
 // Handle worker events (optional)
 worker.on('completed', (job) => {
