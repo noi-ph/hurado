5810ea09653a2a342ab90e93c2883dde6bb07194
Cisco <gfmortega@hotmail.com>
2025-03-15 03:01:25 +0800

    undo buggy bugfix

--- src/common/latex/latex_render.tsx
+++ src/common/latex/latex_render.tsx
@@ -240,25 +240,19 @@ function LatexNodeEnvironmentX({ node, source }: LatexNodeProps<LatexNodeEnviron
       // (it would be arranged vertically by the flex-col)
       return (
         <div className="flex flex-col items-center text-center">
-          <div className="w-full">
-            {renderEnvironmentContent(node, source)}
-          </div>
+          {renderEnvironmentContent(node, source)}
         </div>
       );
     case "flushright":
       return (
         <div className="flex flex-col items-right text-right">
-          <div className="w-full">
-            {renderEnvironmentContent(node, source)}
-          </div>
+          {renderEnvironmentContent(node, source)}
         </div>
       );
     case "flushleft":
       return (
         <div className="flex flex-col items-left text-left">
-          <div className="w-full">
-            {renderEnvironmentContent(node, source)}
-          </div>
+          {renderEnvironmentContent(node, source)}
         </div>
       );
     case "enumerate":
