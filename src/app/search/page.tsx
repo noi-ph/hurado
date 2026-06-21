1726c1cc96f0c6aa18cb76e55217a6cad4353b56
Dan Baterisna <8340533+danbaterisna@users.noreply.github.com>
2026-06-22 00:37:44 +0800

    Add search bar to problem set page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,11 +4,10 @@ import { DefaultLayout } from "client/components/layouts/default_layout";
 import { TaskScoredSummaryDTO } from "common/types";
 import { getSession } from "server/sessions";
 import { TaskCard } from "client/components/cards/common_card";
+import { ProblemSearchBar } from "client/components/problem_search_bar/problem_search_bar";
 
 async function getSearchResults(queryString: string, userId: string | null): Promise<TaskScoredSummaryDTO[]> {
-  const tasks = db.transaction().execute(async (trx) => {
-    const tasks = await trx
-      .selectFrom("tasks")
+  const tasks = db.selectFrom("tasks")
       .leftJoin("overall_verdicts", (join) =>
         join.onRef("overall_verdicts.task_id", "=", "tasks.id")
           .on("overall_verdicts.user_id", "=", userId)
@@ -19,12 +18,10 @@ async function getSearchResults(queryString: string, userId: string | null): Pro
         "tasks.description",
         "overall_verdicts.score_overall",
         "overall_verdicts.score_max"
-      ]).execute();
+      ])
+      .where('tasks.title', 'ilike', sql<string>`CONCAT('%', ${queryString}::text, '%')`).execute();
 
-    return tasks;
-  });
-
-  return tasks
+  return tasks;
 }
 
 export async function SearchResultsPage({ searchParams }: { searchParams: Promise<{ query: string }> }) {
@@ -34,13 +31,19 @@ export async function SearchResultsPage({ searchParams }: { searchParams: Promis
   const params = await searchParams;
   const queryString = params.query;
 
+
   const results = await getSearchResults(queryString, userId);
   return (
     <DefaultLayout>
-      <p> {results.length} results found </p>
-      {results.map((task) => (
-        <TaskCard key={task.slug} task={task} />
-      ))}
+      <ProblemSearchBar />
+      <div className="mt-2 mb-4">
+        {results.length} result(s) found for task named &quot; {queryString} &quot;
+      </div>
+      <div className="flex flex-col items-center gap-4 mt-8">
+        {results.map((task) => (
+          <TaskCard key={task.slug} task={task} />
+        ))}
+      </div>
     </DefaultLayout>
   );
 }
